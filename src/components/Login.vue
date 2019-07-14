ec469817b8c318ed680f892d3d44fbd103d84cbb
Will Engel <willengel4@gmail.com>
2019-07-14 18:33:14 -0400

    cleanup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,23 +13,16 @@ export default {
     },
     methods: {
         signIn() {
-            debugger;
             const provider = new firebase.auth.GoogleAuthProvider()
+            
             firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
+                var token = result.credential.accessToken;
+                var user = result.user;
             }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
             });       
         }
     }
