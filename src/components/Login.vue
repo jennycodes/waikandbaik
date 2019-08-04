34be4861aadec6689819c7e298ee5ce58c0021a2
Engel, William <willengel4@gmail.com>
2019-08-03 22:38:47 -0400

    Create Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button v-on:click="signIn">Sign In</button>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'Login',
+    data() {
+        return {};
+    },
+    methods: {
+        signIn() {
+            const provider = new firebase.auth.GoogleAuthProvider()
+            
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                var token = result.credential.accessToken;
+                var user = result.user;
+                console.log(user.uid);
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+            });       
+        }
+    }
+}
+</script>
+
+
+
+<style>
+</style>
